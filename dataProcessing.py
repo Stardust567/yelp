006d3c65a48ff846d3aea5a70a87dc1ee11e1da3
ChordWiz@swc_20180076 <stardust1084062596@gmail.com>
2019-08-05 05:44:12 +0800

    nltk + wordcloud

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,42 +1,24 @@
 import pandas as pd
-df = pd.read_csv('test.csv', engine='python')
-'''
-df = pd.read_csv('test.csv', engine='python')
-word = df.ix[877]
-print(word)
-print(word['address'])
-print(word['name'])
-print(type(word['address']))
-if(type(word['address'])==type(1.1)):
-    print('*********************************')
-print(word['date'])
-#print(word['review'])
 
-for i in range(1659):
-    word = df.ix[i]
-    word['name'] = word['name'].strip('\n')
-    if (type(word['address']) == type(1.1)):
-        temp = df.ix[i-1]
-        word['address'] = temp['address']
-    else:
-        word['address'] = word['address'].strip('\n')
-        word['address'] = (word['address'].split(','))[0]
-    word['date'] = word['date'].strip('\n')
-    word['review'] = word['review'].strip('\n')
-df.to_csv('test.csv')
-'''
-df['name'] = df['name'].fillna('hhh')
-list = df[(df.name=='hhh')].index.tolist()
-print(list)
-df = df.drop(list)
-df['address'] = df['address'].fillna('hhh')
-list = df[(df.address=='hhh')].index.tolist()
-df = df.drop(list)
-df['date'] = df['date'].fillna('hhh')
-list = df[(df.date=='hhh')].index.tolist()
-df = df.drop(list)
-df['review'] = df['review'].fillna('hhh')
-list = df[(df.review=='hhh')].index.tolist()
-df = df.drop(list)
-print(df.info())
-df.to_csv('process.csv')
+def dataProcess(send_url, recv_url):
+    df = pd.read_csv(send_url, engine='python')
+
+    df['name'] = df['name'].fillna('hhh')
+    list = df[(df.name == 'hhh')].index.tolist()
+    df = df.drop(list)
+
+    df['address'] = df['address'].fillna('hhh')
+    list = df[(df.address == 'hhh')].index.tolist()
+    df = df.drop(list)
+
+    df['date'] = df['date'].fillna('hhh')
+    list = df[(df.date == 'hhh')].index.tolist()
+    df = df.drop(list)
+
+    df['review'] = df['review'].fillna('hhh')
+    list = df[(df.review == 'hhh')].index.tolist()
+    df = df.drop(list)
+
+    df['review'] = df['review'].str.replace("[^a-zA-Z#]", " ")
+    print(df.info())
+    df.to_csv(recv_url)
--- yelpNLP.py
+++ yelpNLP.py
@@ -1,3 +1,43 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from wordcloud import WordCloud
 
-df = pd.read_csv('process.csv', engine='python')
+def cloud(text, label=1):
+    if(label==1):
+        wordcloud = WordCloud(width=1000, height=500, background_color='white', max_words=1000,
+                              colormap='plasma').generate(text)
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.show()
+    else:
+        wordcloud = WordCloud(width=1000, height=500, max_words=1000).generate(text)
+        plt.imshow(wordcloud)
+        plt.axis("off")
+        plt.show()
+
+
+df = pd.read_csv('processed.csv', engine='python')
+review = df['review']
+neg_list = []
+neu_list = []
+pos_list = []
+
+for i in range(len(review)):
+    text = review[i]
+    sid = SentimentIntensityAnalyzer()
+    ss = sid.polarity_scores(text)
+    neg_list.append(ss['neg'])
+    neu_list.append(ss['neu'])
+    pos_list.append(ss['pos'])
+
+data = {'name': df['name'],
+        'address': df['address'],
+        'date': df['date'],
+        'neg':pd.Series(neg_list),
+        'neu':pd.Series(neu_list),
+        'pos':pd.Series(pos_list),
+        }
+dataMat = pd.DataFrame(data)
+print(dataMat)
+dataMat.to_csv('dataMat.csv')
